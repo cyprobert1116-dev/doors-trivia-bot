7a211c6da99e7dea9a406886b89b7cc7d4058f2a
cyprobert1116-dev <cyprobert1116@gmail.com>
2026-02-03 20:50:38 -0600

    tiny renderfix

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log("Web server running on port", PORT);
+});
+
 global.BOT_RUNNING = true;
 
 const { Client, GatewayIntentBits } = require("discord.js");
